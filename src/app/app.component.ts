51b660ffa48069406c9cfa4f4f4471ad7ecbe693
Bernd Vierhuf <bernd.vierhuf@siemens.com>
2018-01-31 10:05:22 +0100

    First Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular von Bernd: Getting Started';
+  title = 'Angular von BV: Getting Started';
 }
