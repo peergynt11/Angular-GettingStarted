80d71ef35b886b18f25f482caa381f23391bb794
Bernd Vierhuf <bernd.vierhuf@siemens.com>
2018-02-06 10:48:40 +0100

    more changes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './products';
 
 @Component({
@@ -6,13 +6,25 @@ import { IProduct } from './products';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'First Product List';
     imageWidth: number = 50;
     imageMargin: number =2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] =
+    _listFilter: string = 'cart';
+    
+    get listFilter() :string {
+        return this._listFilter
+    }
+
+    set listFilter(value: string ) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    
+    filteredProducts: IProduct[];
+    products: IProduct[] =
     [
         {
             "productId": 1,
@@ -66,7 +78,24 @@ export class ProductListComponent{
         }
     ];
 
+
+    constructor()   {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';   
+    }
+
     toggleImage(): void{
         this.showImage = !this.showImage;
     }
+
+    ngOnInit(): void {
+        console.log('in On Init');
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        return this.products.filter(( product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1 );
+    }
+
 }
+
